9a79f49fb2c604293cfe62519af06276345665fc
Hannes <reihe0@snthetik.com>
2018-10-03 20:09:30 +0200

    initial commit

---
+++ src/MainServer.java
@@ -0,0 +1,51 @@
+
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class MainServer {
+	static ServerSocket welcomeSocket;
+	static ArrayList<Socket> allConnections = new ArrayList<Socket>();
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String clientSentence;
+		  String capitalizedSentence;
+		  welcomeSocket = new ServerSocket(25565);
+
+		  while (true) {
+			  Thread st = new ServerThread();
+			  ((ServerThread)st).connectionSocket=welcomeSocket.accept();
+			  allConnections.add(((ServerThread)st).connectionSocket);
+			  st.start();
+		  
+		  }
+	}
+	
+	public static void killServer() {
+		try {
+			welcomeSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.exit(0);
+	}
+	
+	public static void addMessage(String message) {
+		for(Socket s : allConnections) {
+		DataOutputStream outToClient;
+		if(s.isConnected()&&!s.isClosed()) {
+			try {
+				outToClient = new DataOutputStream(s.getOutputStream());
+				outToClient.writeBytes(message);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		   
+		}
+	}
+
+}
---
+++ src/ServerThread.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+	public Socket connectionSocket;
+	public String username;
+@Override
+public void run(){
+	String clientSentence;
+	  String capitalizedSentence;
+	  int messagecount=0;
+    try {
+    	
+    	System.out.println("accepting");
+		   BufferedReader inFromClient =
+		    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+		   DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+		  
+		   clientSentence = inFromClient.readLine();
+		   username=clientSentence;
+		   while(!clientSentence.equalsIgnoreCase("#disconnect#")) {
+		   System.out.println("Received: " + clientSentence);
+		   if(clientSentence.equalsIgnoreCase("killserver")) {
+			   MainServer.killServer();
+		   }
+		   //capitalizedSentence = clientSentence.toUpperCase() + "\n";
+		   if(messagecount==0) {
+		   capitalizedSentence = "Hallo " + clientSentence + "!\n";
+		   MainServer.addMessage(capitalizedSentence);
+		   }else {
+			   capitalizedSentence = username + ": " + clientSentence + "\n";
+			   MainServer.addMessage(capitalizedSentence);
+		   }
+		   clientSentence = inFromClient.readLine();
+		   messagecount++;
+		   }
+		   connectionSocket.close();
+    }catch (Exception e){
+        System.out.println(e.toString());
+       
+    }
+    
+}
+}
